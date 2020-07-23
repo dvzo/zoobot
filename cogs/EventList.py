15708c5970f854905f65a1b207257dcf183f6aeb
dvzo <60148147+dvzo@users.noreply.github.com>
2020-07-22 20:49:42 -0400

    removed reactions from events, and changed the emote listner to use a dictionary - only sends emote when the text version is called

--- cogs/EventList.py
+++ cogs/EventList.py
@@ -5,23 +5,23 @@ from discord.ext import commands
 
 # list of events
 class EventList(commands.Cog):
-    emote_list = ['<:KEKW:653684411069366288>',
-                  '<:Pog:600124616559689738>',
-                  '<:PogU:600124656598646806>',
-                  '<:PogChamp:556705814895788042>',
-                  '<:POGGERS:600124637275357194>',
-                  '<:BibleThump:539378160089956353>',
-                  '<:scamTicket:727675345439817728>',
-                  '<:LUL:550582087485489173>',
-                  '<:HYPERS:635856198540722196>',
-                  '<:peepoSad:720115528445984828>',
-                  '<:peepoHappy:720115503645065328>',
-                  '<:Gasp:557506419277168640>',
-                  '<:FeelsBadMan:539400621334200320>',
-                  '<:monkaS:556704950348939265>',
-                  '<:monkaHmm:563891606546022400>',
-                  '<:FeelsBirthdayMan: 691778442416619551>'
-                  ]
+    emote_dict = {'KEKW': '<:KEKW:653684411069366288>',
+                  'Pog': '<:Pog:600124616559689738>',
+                  'PogU': '<:PogU:600124656598646806>',
+                  'PogChamp': '<:PogChamp:556705814895788042>',
+                  'POGGERS': '<:POGGERS:600124637275357194>',
+                  'BibleThump': '<:BibleThump:539378160089956353>',
+                  'scamTicket': '<:scamTicket:727675345439817728>',
+                  'LUL': '<:LUL:550582087485489173>',
+                  'HYPERS': '<:HYPERS:635856198540722196>',
+                  'peepoSad': '<:peepoSad:720115528445984828>',
+                  'peepoHappy': '<:peepoHappy:720115503645065328>',
+                  'Gasp': '<:Gasp:557506419277168640>',
+                  'FeelsBadMan': '<:FeelsBadMan:539400621334200320>',
+                  'monkaS': '<:monkaS:556704950348939265>',
+                  'monkaHmm': '<:monkaHmm:563891606546022400>',
+                  'FeelsBirthdayMan': '<:FeelsBirthdayMan: 691778442416619551>'
+                  }
 
     def __init__(self, client):
         self.client = client
@@ -46,9 +46,9 @@ class EventList(commands.Cog):
 
         # search through the emote list
         # if the message is just an emoji, react to it with that same emoji, and send it back as well
-        if message.content in self.emote_list:
-            await message.add_reaction(message.content)
-            await message.channel.send(message.content)
+        if message.content in self.emote_dict.keys():
+            emote_value = self.emote_dict[message.content]  # grab the value from the message as a key
+            await message.channel.send(emote_value)
 
 
 # setups this cog
